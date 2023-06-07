cd298921b58f07b0d2a75aec0a8534595cf32845
TOUFIQUER RAHMAN <toufiquerabc@gmail.com>
2023-06-08 01:21:03 +0600

    add route and page

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,14 +2,26 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Home from "../page/Home/Home";
 import LayOut from "../layOut/LayOut";
-import Blog from "../page/Blog/Blog";
+import Blogs from "../page/Blogs/Blogs";
+import AddBlog from "../page/AddBlog/AddBlog";
+import UpdateBlog from "../page/UpdateBlog/UpdateBlog";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <LayOut element={<Home />} />,
   },
   {
-    path: "/blog",
-    element: <LayOut element={<Blog />} />,
+    path: "/blogs",
+    element: <LayOut element={<Blogs />} />,
+  },
+
+  {
+    path: "/addBlog",
+    element: <LayOut element={<AddBlog />} />,
+  },
+
+  {
+    path: "/updateBlog",
+    element: <LayOut element={<UpdateBlog />} />,
   },
 ]);
